841e79abee291871a256021d3ed75ef051c598cd
jamez2698 <146166328+jamez2698@users.noreply.github.com>
2023-11-09 08:12:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $request_uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : null;
 
 // Define routes in a separate configuration file
 $routes = [
-    '/myporfolio/' => '/modules/body.php',
-    '/myporfolio/home/' => '/modules/home.php',
-    '/myporfolio/porfolio/' => '/modules/porfolio.php',
+    '/jlaporfolio/' => '/modules/body.php',
+    '/jlaporfolio/home/' => '/modules/home.php',
+    '/jlaporfolio/porfolio/' => '/modules/porfolio.php',
 ];
 
 // Default route
